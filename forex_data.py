bcca04eecca40528aaa69e7a6aed60c9d62ed580
CheranMahalingam <65926174+CheranMahalingam@users.noreply.github.com>
2020-12-30 15:31:04 -0500

    initialize forex data script

---
+++ forex_data.py
@@ -0,0 +1,13 @@
+import finnhub
+import requests
+import pandas as pd
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+finnhub_client = finnhub.Client(api_key=os.getenv("PROJECT_API_KEY"))
+
+res = finnhub_client.forex_candles('OANDA:EUR_USD', '60', 1609200000, 1609357882)
+#print(finnhub_client.forex_rates(base='USD'))
+df = pd.DataFrame(res)
+print(df.describe())
