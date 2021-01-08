df98cbc107d753c7a96d0d38326559409a7189bb
CheranMahalingam <65926174+CheranMahalingam@users.noreply.github.com>
2021-01-07 19:59:27 -0500

    Modified Twitter webscraper to query by month to increase tweets collected

--- forex_webscraper/forex_webscraper/spiders/twitter_webscraper.py
+++ forex_webscraper/forex_webscraper/spiders/twitter_webscraper.py
@@ -15,34 +15,36 @@ chrome_options.add_argument("--headless")
 
 driver = webdriver.Chrome(executable_path=browser_path, options=chrome_options)
 
-urls = ["https://www.twitter.com/ReutersGMF?lang=en",
-        "https://twitter.com/forexcom?lang=en",
-        "https://twitter.com/FXstreetNews?lang=en"]
+def search(users):
+    for user in users:
+        data = {}
+        data['headline'] = []
+        data['date'] = []
 
-data = {}
-data['headline'] = []
-data['date'] = []
+        for year in range(2018, 2021):
+            for month in range(1, 13):
+                feb_days = "29" if year%4 == 0 else "28"
+                days_per_month = {1: "31", 2: feb_days, 3: "31", 4: "30", 5: "31", 6: "30", 7: "31", 8: "31", 9: "30", 10: "31", 11: "30", 12: "31"}
+                str_month = str(month) if month > 9 else "0" + str(month)
+                url = "https://www.twitter.com/search?lang=en&q=(from%3A" + user + ")%20until%3A" + str(year) + "-" + str_month \
+                        + "-" + days_per_month[month] + "%20since%3A" + str(year) + "-" + str_month + "-01%20&src=typed_query&f=live"
+                scrape_data(url, data)
+        
+        store_data(user, data)
 
-def sleep_for(opt1, opt2):
-    time_for = random.uniform(opt1, opt2)
-    time_for_int = int(round(time_for))
-    sleep(abs(time_for_int - time_for))
-    for i in range(time_for_int, 0, -1):
-        sleep(1)
-
-def scrape_data(url):
+def scrape_data(url, data):
     tweet_count = 0
-    TWEET_MAX = 1
+    TWEET_MAX = 3000
 
     driver.get(url)
-    sleep_for(10, 15)
+    sleep_for(5, 9)
 
     tweet_store = set()
     post_element_xpath = "//div[@data-testid='tweet']"
     current_time = time()
 
     while tweet_count < TWEET_MAX:
-        if time() - current_time > 120:
+        if time() - current_time > 20:
             break
         post_list = driver.find_elements_by_xpath(post_element_xpath)
         for post in post_list:
@@ -55,30 +57,35 @@ def scrape_data(url):
 
             if post_date != 0 and post_text not in tweet_store:
                 current_time = time()
-                print(tweet_count)
                 data['headline'].append(post_text)
                 data['date'].append(post_date)
                 tweet_store.add(post_text)
                 tweet_count += 1
+                print(tweet_count)
                 if tweet_count == TWEET_MAX:
                     break
         
         new_post_list = driver.find_elements_by_xpath(post_element_xpath)
-        scroll_count = 0
 
         while new_post_list == post_list:
-            if(time() - current_time) > 120:
+            if(time() - current_time) > 20:
                 tweet_count == TWEET_MAX
                 break
+            
             driver.execute_script("window.scrollBy(0, 200);")
             new_post_list = driver.find_elements_by_xpath(post_element_xpath)
-            scroll_count += 1
 
-        sleep_for(1, 3)
-        store_data()
+        sleep_for(1, 2)
 
-def store_data():
-    with open('twitter.json', 'w') as outfile:
+def sleep_for(opt1, opt2):
+    time_for = random.uniform(opt1, opt2)
+    time_for_int = int(round(time_for))
+    sleep(abs(time_for_int - time_for))
+    for i in range(time_for_int, 0, -1):
+        sleep(1)
+
+def store_data(user, data):
+    with open(str(user) + "_historical.json", 'w') as outfile:
         json.dump(data, outfile)
 
 def close_driver():
@@ -86,6 +93,6 @@ def close_driver():
 
 
 if __name__ == "__main__":
-    for i in urls:
-        scrape_data(i)
+    users = ["ReutersGMF", "forexcom", "FXstreetNews", "FTMarkets", "markets", "WSJmarkets"]
+    search(users)
     close_driver()
