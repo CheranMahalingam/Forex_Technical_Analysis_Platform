b5398154ee4e5d580fd35b1a4d05412756395497
CheranMahalingam <65926174+CheranMahalingam@users.noreply.github.com>
2021-01-19 00:32:34 -0500

    Historical data for major economic indicators

--- forex_data.py
+++ forex_data.py
@@ -6,7 +6,9 @@ load_dotenv()
 
 finnhub_client = finnhub.Client(api_key=os.getenv("PROJECT_API_KEY"))
 
-res = finnhub_client.forex_candles('OANDA:EUR_USD', '60', 1609200000, 1609357882)
-print(finnhub_client.forex_rates(base='USD'))
+res = finnhub_client.forex_candles('OANDA:EUR_USD', '60', 1514764800, 1609459200)
+print(res)
 df = pd.DataFrame(res)
 print(df.describe())
+
+print(finnhub_client.forex_exchanges())
