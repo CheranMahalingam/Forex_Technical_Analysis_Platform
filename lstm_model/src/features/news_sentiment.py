5749a8525c99158dd210bc0b8cfb3b8e325fd217
CheranMahalingam <65926174+CheranMahalingam@users.noreply.github.com>
2021-03-11 22:32:27 -0500

    Formatted & fixed dataframe bugs for preprocessing

---
+++ lstm_model/src/features/news_sentiment.py
@@ -0,0 +1,172 @@
+"""Feature engineering for sentiment around a currency using news headlines."""
+
+from datetime import datetime
+from functools import reduce
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import pandas as pd
+
+
+def news_unpacking(org):
+    """
+    Reads historical news headlines from a json file and writes them into a dataframe.
+
+    Args:
+        org: String representing the news organization from which news is read
+
+    Returns:
+        Dataframe containing news headlines, time, and summaries.
+    """
+    org_news = pd.read_json("../../data/raw/news/{}_historical.json".format(org))
+    headline_arr = []
+    date_arr = []
+    summary_arr = []
+
+    for month in range(org_news["headline"].count()):
+        for headline in org_news["headline"][month]:
+            headline_arr.append(headline)
+        for date in org_news["date"][month]:
+            date_arr.append(date)
+        if "summary" in org_news:
+            for summary in org_news["summary"][month]:
+                summary_arr.append(summary)
+    if summary_arr:
+        org_df = pd.DataFrame({"Time": date_arr, "Headline": headline_arr, "Summary": summary_arr})
+    else:
+        org_df = pd.DataFrame({"Time": date_arr, "Headline": headline_arr,})
+    org_df["Time"] = org_df["Time"].transform(
+        lambda time: datetime.utcfromtimestamp(time).strftime("%Y-%m-%d %H:%M:00"))
+    org_df.sort_values(by=["Time"], inplace=True)
+    org_df = org_df.reset_index(drop=True)
+    return org_df
+
+def news_merge(news):
+    """
+    Merges array of news dataframes into one.
+
+    Args:
+        news: Array of news dataframes
+
+    Returns:
+        Single dataframe containing all headlines from different news sources.
+    """
+    if len(news) == 0:
+        return None
+    if len(news) == 1:
+        return news[0]
+    merged_news = reduce(
+        lambda left, right: pd.merge(left, right, how="outer", on=["Time", "Headline"]), news)
+    merged_news.sort_values(by=["Time"], inplace=True)
+    merged_news = merged_news.reset_index(drop=True)
+    return merged_news
+
+def generate_sentiment_score(news):
+    """
+    Generates a sentiment score for news headlines and summaries.
+
+    Args:
+        news: Dataframe containing time, headline, and summary of news
+
+    Returns:
+        Dataframe with additional column containing sentiment score.
+    """
+    sid = SentimentIntensityAnalyzer()
+    score = []
+    for headline, summary in zip(news.Headline, news.Summary):
+        if pd.isna(summary):
+            text = headline
+            score.append(sid.polarity_scores(text)["compound"])
+        else:
+            text = headline + summary
+            score.append(sid.polarity_scores(text)["compound"])
+    news_score = pd.DataFrame({"News_Sentiment": score})
+    news["News_Sentiment"] = news_score
+    return news
+
+def currency_sentiment(currencies_dict, news):
+    """
+    Generates csv file containing news sentiment scores for each country according to date.
+
+    Args:
+        currencies_dict: Dictionary holding keywords to indicate that a headline affects the
+                         strength of a currency and whether is positive or negative
+        news: Dataframe containing news headlines with their sentiment scores
+    """
+    country_df = pd.DataFrame()
+    for currency in currencies_dict:
+        for entity in currencies_dict[currency]["positive"]:
+            headline_lower = news["Headline"].transform(lambda headline: headline.lower())
+            summary_lower = news["Summary"].transform(
+                lambda summary: summary.lower() if isinstance(summary) == "object" else summary)
+            currency_df = news[(headline_lower.str.contains(entity)) | \
+                (summary_lower.str.contains(entity))]
+            currency_df = currency_df[{"Time", "News_Sentiment"}]
+            currency_df = currency_df.rename(columns={"News_Sentiment": currency.upper()})
+            if country_df.empty:
+                country_df = currency_df
+            elif not currency.upper() in country_df.columns:
+                country_df = country_df.merge(currency_df, how="outer", on="Time")
+            else:
+                country_df = country_df.merge(
+                    currency_df, how="outer", on=["Time", currency.upper()])
+        for entity in currencies_dict[currency]["negative"]:
+            headline_lower = news["Headline"].transform(lambda headline: headline.lower())
+            summary_lower = news["Summary"].transform(
+                lambda summary: summary.lower() if isinstance(summary) == "object" else summary)
+            currency_df = news[(headline_lower.str.contains(entity)) | \
+                (summary_lower.str.contains(entity))]
+            currency_df = currency_df[{"Time", "News_Sentiment"}]
+            if not currency_df["News_Sentiment"].empty:
+                currency_df["News_Sentiment"] = currency_df["News_Sentiment"].transform(
+                    lambda score: -score)
+            currency_df = currency_df.rename(columns={"News_Sentiment": currency.upper()})
+            if country_df.empty:
+                country_df = currency_df
+            elif not currency.upper() in country_df.columns:
+                country_df = country_df.merge(currency_df, how="outer", on="Time")
+            else:
+                country_df = country_df.merge(
+                    currency_df, how="outer", on=["Time", currency.upper()])
+
+    country_df = combine_dates(country_df)
+
+    time_frame = pd.date_range(start="2018-01-01 22:00:00", freq="1T", end="2020-12-31 21:59:00")
+    time_frame = pd.DataFrame(time_frame, columns=["Time"])
+    time_frame["Time"] = time_frame["Time"].dt.strftime("%Y-%m-%d %H:%M:%S")
+
+    country_df = time_frame.merge(country_df, how="outer", on="Time")
+    country_df = country_df.sort_values(by='Time', ascending=True)
+
+    for currency in currencies_dict:
+        country_df[currency.upper()] = country_df[currency.upper()].rolling(
+            1440, min_periods=1).mean()
+    country_df = country_df.fillna(0)
+
+    country_df.to_csv("../../data/interim/news/news_sentiment.csv", index=False)
+
+def combine_dates(news):
+    """
+    Merge sentiment scores according to date.
+
+    Args:
+        news: Dataframe containing countries and their sentiment scores at a certain time
+
+    Returns:
+        Dataframe with a country's sentiment score with sequential time.
+    """
+    currencies = ["eur", "usd", "jpy", "cad", "gbp", "aud", "nzd", "chf"]
+    length = 1
+    for i in range(1, len(news.index)):
+        current = news.at[i, "Time"]
+        if current == news.at[i - length, "Time"] and i == len(news.index) - 1:
+            for currency in currencies:
+                news.at[i - length, currency.upper()] = news[
+                    currency.upper()].iloc[i - length: i].mean()
+        elif current == news.at[i - length, "Time"]:
+            length += 1
+        elif length > 1:
+            for currency in currencies:
+                news.at[i - length, currency.upper()] = news[
+                    currency.upper()].iloc[i - length: i].mean()
+            length = 1
+    news.drop_duplicates(subset=["Time"], inplace=True)
+    return news
---
+++ lstm_model/src/features/twitter_sentiment.py
@@ -0,0 +1,186 @@
+"""Feature engineering for sentiment score of a currency using tweets."""
+
+from functools import reduce
+import re
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import pandas as pd
+import numpy as np
+
+
+def twitter_dataframe(account):
+    """
+    Converts json file containing an account's tweets to a dataframe.
+
+    Args:
+        account: String representing the name of a twitter account
+
+    Returns:
+        Dataframe containing tweets from an account with post and date.
+    """
+    tweets = pd.read_json("../../data/raw/tweets/{}_historical.json".format(account))
+    headline_arr = []
+    date_arr = []
+
+    for post in range(tweets["headline"].count()):
+        headline_arr.append(tweets["headline"].iloc[post])
+        date_arr.append(tweets["date"].iloc[post])
+    tweets_df = pd.DataFrame({"Time": date_arr, "Post": headline_arr})
+
+    tweets_df["Post"] = clean_tweets(tweets_df["Post"])
+    tweets_df = generate_sentiment_score(tweets_df)
+    tweets_df["Time"] = tweets_df["Time"].dt.strftime("%Y-%m-%d %H:%M:00")
+    return tweets_df
+
+def generate_sentiment_score(tweets):
+    """
+    Generates sentiment scores for each tweet.
+
+    Args:
+        tweets: Dataframe containing tweet date and post
+
+    Returns:
+        Tweet dataframe with additional column for sentiment score.
+    """
+    sid = SentimentIntensityAnalyzer()
+    score = []
+    for post in tweets["Post"]:
+        score.append(sid.polarity_scores(post)["compound"])
+    tweet_score = pd.DataFrame({"Twitter_Sentiment": score})
+    tweets["Twitter_Sentiment"] = tweet_score
+    return tweets
+
+def clean_tweets(tweets):
+    """
+    Strips twitter posts of uninformative special characters.
+
+    Args:
+        tweets: Dataframe containing tweet date, post, and sentiment score
+
+    Returns:
+        Tweet dataframe with machine readable tweets.
+    """
+    # pylint: disable=anomalous-backslash-in-string
+    tweets = np.vectorize(remove_pattern)(tweets, "RT @[\w]*:")
+    tweets = np.vectorize(remove_pattern)(tweets, "@[\w]*")
+    tweets = np.vectorize(remove_pattern)(tweets, "https?://[A-Za-z0-9./]*")
+    tweets = np.core.defchararray.replace(tweets, "[^a-zA-Z]", " ")
+    tweets = np.core.defchararray.replace(tweets, "\n", " ")
+    return tweets
+
+def remove_pattern(input_text, pattern):
+    """
+    Finds patterns in posts and substitutes them with blank space.
+
+    Args:
+        input_text: String representing a twitter post
+        pattern: Regex pattern to search for in twitter post
+
+    Returns:
+        String with pattern stripped.
+    """
+    match = re.findall(pattern, input_text)
+    for i in match:
+        input_text = re.sub(i, "", input_text)
+    return input_text
+
+def tweets_merge(tweet_list):
+    """
+    Merges a list of tweet dataframes.
+
+    Args:
+        tweet_list: Array of tweet dataframes containing time, post, and sentiment scores
+
+    Returns:
+        Single dataframe containing tweets + scores for all accounts.
+    """
+    if len(tweet_list) == 0:
+        return None
+    if len(tweet_list) == 1:
+        return tweet_list[0]
+    merged_tweets = reduce(lambda left, right: pd.merge(
+        left, right, how="outer", on=["Time", "Post", "Twitter_Sentiment"]), tweet_list)
+    merged_tweets.sort_values(by=["Time"], inplace=True)
+    merged_tweets = merged_tweets.reset_index(drop=True)
+    return merged_tweets
+
+def currency_sentiment(currencies_dict, tweets):
+    """
+    Generates csv file containing tweet sentiment scores for each country according to date.
+
+    Args:
+        currencies_dict: Dictionary holding keywords to indicate that a post affects the
+                         strength of a currency and whether is positive or negative
+        tweets: Dataframe containing twitter posts with their sentiment scores
+    """
+    country_df = pd.DataFrame()
+    for currency in currencies_dict:
+        for entity in currencies_dict[currency]["positive"]:
+            tweet_lower = tweets["Post"].transform(lambda post: post.lower())
+            currency_df = tweets[tweet_lower.str.contains(entity)]
+            currency_df = currency_df[{"Time", "Twitter_Sentiment"}]
+            currency_df = currency_df.rename(columns={"Twitter_Sentiment": currency.upper()})
+            if country_df.empty:
+                country_df = currency_df
+            elif not currency.upper() in country_df.columns:
+                country_df = country_df.merge(currency_df, how="outer", on="Time")
+            else:
+                country_df = country_df.merge(
+                    currency_df, how="outer", on=["Time", currency.upper()])
+        for entity in currencies_dict[currency]["negative"]:
+            tweet_lower = tweets["Post"].transform(lambda post: post.lower())
+            currency_df = tweets[tweet_lower.str.contains(entity)]
+            currency_df = currency_df[{"Time", "Twitter_Sentiment"}]
+            currency_df["Twitter_Sentiment"] = currency_df["Twitter_Sentiment"].transform(
+                lambda score: -score)
+            currency_df = currency_df.rename(columns={"Twitter_Sentiment": currency.upper()})
+            if country_df.empty:
+                country_df = currency_df
+            elif not currency.upper() in country_df.columns:
+                country_df = country_df.merge(currency_df, how="outer", on="Time")
+            else:
+                country_df = country_df.merge(
+                    currency_df, how="outer", on=["Time", currency.upper()])
+
+    time_frame = pd.date_range(start="2018-01-01 22:00:00", freq="1T", end="2020-12-31 21:59:00")
+    time_frame = pd.DataFrame(time_frame, columns=["Time"])
+    time_frame["Time"] = time_frame["Time"].dt.strftime("%Y-%m-%d %H:%M:%S")
+    country_df = country_df.reset_index(drop=True)
+    country_df = combine_dates(country_df)
+
+    country_df = time_frame.merge(country_df, how="left", on="Time")
+    country_df = country_df.sort_values(by='Time', ascending=True)
+
+    for currency in currencies_dict:
+        country_df[currency.upper()] = country_df[
+            currency.upper()].rolling(1440, min_periods=1).mean()
+    country_df = country_df.fillna(0)
+
+    country_df.to_csv("../../data/interim/tweets/tweets_sentiment.csv", index=False)
+
+def combine_dates(tweets):
+    """
+    Merge sentiment scores according to date.
+
+    Args:
+        tweets: Dataframe containing countries and their sentiment scores at a certain time
+
+    Returns:
+        Dataframe with a country's sentiment score with sequential time.
+    """
+    currencies = ["eur", "usd", "jpy", "cad", "gbp", "aud", "nzd", "chf"]
+    length = 1
+    for i in range(1, len(tweets.index)):
+        current = tweets.at[i, "Time"]
+        if current == tweets.at[i - length, "Time"] and i == len(tweets.index) - 1:
+            for currency in currencies:
+                tweets.at[i - length, currency.upper()] = tweets[
+                    currency.upper()].iloc[i - length: i].mean()
+        elif current == tweets.at[i - length, "Time"]:
+            length += 1
+        elif length > 1:
+            for currency in currencies:
+                tweets.at[i - length, currency.upper()] = tweets[
+                    currency.upper()].iloc[i - length: i].mean()
+            length = 1
+    tweets.drop_duplicates(subset=["Time"], inplace=True)
+    return tweets
